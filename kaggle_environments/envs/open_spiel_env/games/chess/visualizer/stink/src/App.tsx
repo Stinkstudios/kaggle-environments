1b16993aec35e9081ffc4d6cc3927277b51f5966
Jim Hunt <18527157+Jim-Hunt@users.noreply.github.com>
2026-01-31 00:56:11 +0000

    Pull more data

--- kaggle_environments/envs/open_spiel_env/games/chess/visualizer/stink/src/App.tsx
+++ kaggle_environments/envs/open_spiel_env/games/chess/visualizer/stink/src/App.tsx
@@ -1,45 +1,56 @@
-import { createReplayVisualizer, LegacyAdapter, processEpisodeData } from '@kaggle-environments/core'
+import { createReplayVisualizer, LegacyAdapter } from '@kaggle-environments/core'
 import { useEffect, useRef } from 'react'
 import { create } from 'zustand'
 import { Chessboard } from 'react-chessboard'
 import './App.css'
 
 interface ChessStore {
-  position: string
-  setPosition: (options: any) => void
+  position: string,
+  action: string,
+  legalActions: string
+  setState: (state: any) => void
 }
 
 const useChessStore = create<ChessStore>((set) => ({
   position: '',
-  setPosition: (options: any) => {
-    set({
-      position: options.steps[options.step]
-        .find((el: any) => el.status === "ACTIVE")
-        .observation
-        .observationString
+  action: '',
+  legalActions: '',
+  setState: (state: any) => {
+    const step = state.steps[state.step]
+
+    const active = step.filter((item: any) => item.status === "ACTIVE")
+    active.forEach((item: any) => {
+      set({
+        position: item.observation.observationString,
+        legalActions: item.observation.legalActionStrings?.join(" "),
+      })
+    })
+
+    const inactive = step.filter((item: any) => item.status === "INACTIVE")
+    inactive.forEach((item: any) => {
+      set({
+        action: item.action.actionString,
+      })
     })
   }
 }))
 
 function App() {
-  const { position, setPosition } = useChessStore()
+  const { position, action, legalActions, setState } = useChessStore()
   const controlsRef = useRef(null)
 
   useEffect(() => {
     const app = controlsRef.current!
-    const adapter = new LegacyAdapter(setPosition)
+    const adapter = new LegacyAdapter(setState)
 
-    createReplayVisualizer(app, adapter, {
-      transformer: (replay: any) => {
-        return processEpisodeData(replay, 'no_transformer')
-      }
-    })
+    createReplayVisualizer(app, adapter)
   }, [])
 
   return (
     <div className="container">
       <Chessboard options={{ position }} />
       <div id="controls" ref={controlsRef} />
+      <div><b>{action}</b> {legalActions}</div>
     </div>
   )
 }
