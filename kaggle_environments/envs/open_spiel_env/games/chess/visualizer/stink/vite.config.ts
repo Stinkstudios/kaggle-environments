62f972c9d387187b14efa19f44ec5de0f0a20579
Jim Hunt <18527157+Jim-Hunt@users.noreply.github.com>
2026-02-03 14:56:06 +0000

    Update from v3

--- kaggle_environments/envs/open_spiel_env/games/chess/visualizer/stink/vite.config.ts
+++ kaggle_environments/envs/open_spiel_env/games/chess/visualizer/stink/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig, mergeConfig } from 'vite';
-import react from '@vitejs/plugin-react'
 import baseConfig from '../../../../../../../web/vite.config.base';
 
 // https://vitejs.dev/config/
 export default mergeConfig(baseConfig, defineConfig({
-  plugins: [react()],
 }));
