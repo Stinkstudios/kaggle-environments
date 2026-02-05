831f7f24ed006834ade0e8106a046378b504f055
Jim Hunt <18527157+Jim-Hunt@users.noreply.github.com>
2026-02-05 13:11:40 +0000

    Add types and match data access

--- kaggle_environments/envs/open_spiel_env/games/chess/visualizer/v2/src/App.tsx
+++ kaggle_environments/envs/open_spiel_env/games/chess/visualizer/v2/src/App.tsx
@@ -1,31 +1,34 @@
-import { createReplayVisualizer, LegacyAdapter } from '@kaggle-environments/core';
 import { useEffect, useRef } from 'react';
 import { create } from 'zustand';
 import { Chess } from 'chess.js';
 import { Chessboard } from 'react-chessboard';
+import {
+  createReplayVisualizer,
+  processEpisodeData,
+  LegacyAdapter,
+  LegacyRendererOptions,
+  ReplayData,
+  ChessStep,
+  ChessPlayer,
+} from '@kaggle-environments/core';
 import './App.css';
 
 interface ChessStore {
   chess: Chess;
-  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-  setState: (data: any) => void;
+  setState: (data: LegacyRendererOptions<ChessStep[]>) => void;
 }
 
 const useChessStore = create<ChessStore>((set) => ({
   chess: new Chess(),
 
-  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-  setState: (data: any) => {
-    const step = data.steps.at(data.step);
-    // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-    const move = step.find((element: any) => element.action.actionString);
+  setState: (data: LegacyRendererOptions<ChessStep[]>) => {
+    const step = data.steps.at(data.step)!;
+    const move = step.players.find((element: ChessPlayer) => element.isTurn);
 
     if (move) {
-      const history = data.replay.info.stateHistory;
-      const index = history.indexOf(move.observation.observationString);
-
-      const chess = new Chess(history.at(index - 1));
-      chess.move(move.action.actionString);
+      const history = data.replay.info!.stateHistory;
+      const chess = new Chess(history.at(data.step));
+      chess.move(move.actionDisplayText!);
 
       set({ chess });
     }
@@ -38,9 +41,10 @@ function App() {
 
   useEffect(() => {
     const app = controlsRef.current!;
-    const adapter = new LegacyAdapter(setState);
+    const adapter = new LegacyAdapter<ChessStep[]>(setState);
+    const transformer = (replay: ReplayData) => processEpisodeData(replay, 'open_spiel_chess');
 
-    createReplayVisualizer(app, adapter);
+    createReplayVisualizer(app, adapter, { transformer });
   }, [setState]);
 
   return (
